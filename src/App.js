c0e4f076a1807ca504a6f69b91565c96e1aa1ccb
Imikan10 <basseyimikan10@gmail.com>
2024-04-30 09:57:17 +0100

    questions tab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import { names } from './data'
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState({})
+
+  const toggleParagraph = (index) => {
+    setShowParagraph({ ...showParagraph, [index]: !showParagraph[index] })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        <h1>questions</h1>
+
+        {names.map((quest) => {
+          const { question, index } = quest
+          return (
+            <article className='question'>
+              <div className='quest-container' key={index}>
+                <h5>{question}</h5>
+
+                <button
+                  className='question-btn'
+                  onClick={() => toggleParagraph(index)}
+                >
+                  {showParagraph[index] ? (
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      strokeWidth='0'
+                      t='1551322312294'
+                      viewBox='0 0 1024 1024'
+                      version='1.1'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <defs></defs>
+                      <path d='M152 486q0-6 6-10l232-232q6-6 16-6t16 6l232 232q6 6 6 16t-6 16l-20 20q-6 6-16 6t-16-6L512 344l-204 204q-6 6-16 6t-16-6l-20-20q-6-6-6-16Z'></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      stroke-width='0'
+                      t='1551322312294'
+                      viewBox='0 0 1024 1024'
+                      version='1.1'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <defs></defs>
+                      <path d='M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z'></path>
+                      <path d='M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z'></path>
+                    </svg>
+                  )}
+                </button>
+              </div>
+              {showParagraph[index] && (
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ratione mollitia nobis qui molestias ad eveniet, provident
+                  iure nihil? Nobis amet distinctio praesentium eaque ipsam
+                  magnam, aliquid et ducimus fugit aliquam.
+                </p>
+              )}
+            </article>
+          )
+        })}
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const names = [
+  {
+    question: 'do i have to allow the use of cookies?',
+    index: 1,
+  },
+  {
+    question: 'how do i change my page password?',
+    index: 2,
+  },
+  {
+    question: 'what is bankID?',
+    index: 3,
+  },
+  {
+    question: 'whose birth number can i use?',
+    index: 4,
+  },
+  {
+    question: 'when was chelsea football club founded?',
+    index: 5,
+  },
+]
